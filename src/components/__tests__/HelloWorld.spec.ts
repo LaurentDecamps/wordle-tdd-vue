925f56e438a18df6b9396e922cac50dbf021d14e
Décamps Laurent <laurent.decamps@diagngrow.com>
2024-02-09 00:05:13 +0100

    refactor: :recycle: add playerMakeAGuess function in test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -11,22 +11,22 @@ beforeEach(() => {
   
 })
 
+const playerMakeAGuess = async (guess: string) => {
+  const guessInput = wrapper.find('input[type="text"]');
+  
+  await guessInput.setValue(guess);
+  await guessInput.trigger('keydown.enter');
+};
+
 describe('WordleBoard', () => {
   test("A victory message appears when the user makes a guess taht matches the word of the day", async () => {
-    const guessInput  = wrapper.find('input[type="text"]');
-    
-    await guessInput.setValue(WORD_OF_THE_DAY_TO_TEST)
-    await guessInput.trigger('keydown.enter')
+    await playerMakeAGuess(WORD_OF_THE_DAY_TO_TEST);
 
-    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
-  })
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE);
+  })  
 
   test("a defeat message appears if the user makes a guess that is incorrect", async () => {
-
-    const guessInput  = wrapper.find('input[type="text"]');
-    
-    await guessInput.setValue(WRONG_WORD_TO_TEST)
-    await guessInput.trigger('keydown.enter')
+    await playerMakeAGuess(WRONG_WORD_TO_TEST);   
 
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
