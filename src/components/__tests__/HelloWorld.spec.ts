d40039e01b42dcbbc5ea19a8c4463d0e5cfed829
Décamps Laurent <laurent.decamps@diagngrow.com>
2024-02-09 18:48:41 +0100

    fix : put wrapper type to any

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -4,16 +4,16 @@ import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '@/settings'
 
 const WORD_OF_THE_DAY_TO_TEST = "TESTS"
 const WRONG_WORD_TO_TEST = "WRONG"
-let wrapper;
+let wrapper: any;
 
 beforeEach(() => {
   wrapper = mount(WordleBoard, { props: { wordOfTheDay: WORD_OF_THE_DAY_TO_TEST } })
-  
+
 })
 
 const playerMakeAGuess = async (guess: string) => {
   const guessInput = wrapper.find('input[type="text"]');
-  
+
   await guessInput.setValue(guess);
   await guessInput.trigger('keydown.enter');
 };
@@ -23,17 +23,17 @@ describe('WordleBoard', () => {
     await playerMakeAGuess(WORD_OF_THE_DAY_TO_TEST);
 
     expect(wrapper.text()).toContain(VICTORY_MESSAGE);
-  })  
+  })
 
   test("a defeat message appears if the user makes a guess that is incorrect", async () => {
-    await playerMakeAGuess(WRONG_WORD_TO_TEST);   
+    await playerMakeAGuess(WRONG_WORD_TO_TEST);
 
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
 
   test("no end-of-game message appears if the user has not yet made a guess", async () => {
-    const guessInput  = wrapper.find('input[type="text"]');
-    
+    const guessInput = wrapper.find('input[type="text"]');
+
     await guessInput.setValue(WRONG_WORD_TO_TEST)
 
     expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
